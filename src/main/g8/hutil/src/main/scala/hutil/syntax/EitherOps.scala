f4f58c77da02cd7174f82b602a9ce0d7c3f5dec0
Hermann Hueck <hermann.hueck@mailbox.org>
2020-06-13 23:28:56 +0200

    extend EitherMappingSyntax with biMap

--- src/main/g8/hutil/src/main/scala/hutil/syntax/EitherOps.scala
+++ src/main/g8/hutil/src/main/scala/hutil/syntax/EitherOps.scala
@@ -3,10 +3,15 @@ package syntax
 
 package object either {
 
-  final implicit class EitherSyntaxLeftMap[L, R](private val either: Either[L, R]) extends AnyVal {
-    @inline def leftMap[L2](f: L => L2): Either[L2, R] = either match {
-      case Left(a)      => Left(f(a))
-      case r @ Right(_) => r.asInstanceOf[Either[L2, R]]
-    }
+  implicit final class EitherMappingSyntax[L, R](private val either: Either[L, R]) extends AnyVal {
+
+    @inline def leftMap[L2](f: L => L2): Either[L2, R] =
+      either match {
+        case Left(a)      => Left(f(a))
+        case r @ Right(_) => r.asInstanceOf[Either[L2, R]]
+      }
+
+    @inline def biMap[L2, R2](fLeft: L => L2, fRight: R => R2): Either[L2, R2] =
+      either.leftMap(fLeft).map(fRight)
   }
 }
